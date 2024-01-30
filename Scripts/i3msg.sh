9653e76c3a05cce38f2e4d860322da41b86db438
DS <saridakisbitos@gmail.com>
2024-01-30 06:20:51 +0200

    more i3 beautifying

--- Scripts/i3msg.sh
+++ Scripts/i3msg.sh
@@ -1,12 +1,12 @@
 
 pdf_script=$HOME/StunningFiles/Scripts/pdf.sh
 
-i3-msg "workspace 1; workspace 1👨‍💻terminal; exec i3-sensible-terminal"
+i3-msg "workspace 1; workspace 1👨‍💻; exec i3-sensible-terminal"
 sleep 1
-i3-msg "workspace 2🏄browser; exec brave-browser; exec brave-browser --new-window"
+i3-msg "workspace 2🏄; exec brave-browser; exec brave-browser --new-window"
 sleep 1
-i3-msg "workspace 3📖pdf; exec $pdf_script"
+i3-msg "workspace 3📖; exec $pdf_script"
 sleep 1
-i3-msg "workspace 4📹youtube; exec brave-browser --new-window youtube.com"
+i3-msg "workspace 4📹; exec brave-browser --new-window youtube.com"
 sleep 1
-i3-msg "workspace 5💬signal; exec signal-desktop"
+i3-msg "workspace 5💬; exec signal-desktop"
